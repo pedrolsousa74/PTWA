991e943f387cf2d9ae6a97464eef31ce5c45b6af
Rodrigo <rodrigo.dferreira@ua.pt>
2025-03-30 14:54:17 +0100

    Setup inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post-it</title>
+</head>
+<body>
+    <h1>Bem-vindo ao Post-it</h1>
+    <p>Clica no botao para veres os arigos.</p>
+
+    <a href="" class="btn">
+        <button>Ver artigos</button>
+    </a>
+</body>
+</html>
